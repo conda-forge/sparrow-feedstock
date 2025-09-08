97d5797faaaa2c3359103855f98e0b6089314bd0
Alexis Placet <alexis.placet@quantstack.net>
2025-09-08 11:03:09 +0200

    Add sparrow-dev

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-cmake ${CMAKE_ARGS} -DBUILD_TESTS=OFF \
+if /I "%PKG_NAME%" == "sparrow" (
+      cmake ${CMAKE_ARGS} -DBUILD_TESTS=OFF \
+            -DCMAKE_INSTALL_PREFIX=$PREFIX \
+            -DCMAKE_INSTALL_LIBDIR=lib \
+            $SRC_DIR
+)
+if /I "%PKG_NAME%" == "sparrow-dev" (
+      cmake ${CMAKE_ARGS} -DBUILD_TESTS=OFF \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_INSTALL_LIBDIR=lib \
+      -DCREATE_JSON_READER_TARGET=ON \
       $SRC_DIR
-
+)
 make install
+
