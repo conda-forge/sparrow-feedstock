f1d5a6f23ebf2b298b8aadaf164dd826f857b99c
Johan Mabille <johan.mabille@gmail.com>
2025-10-20 15:34:34 +0200

    Fix

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,13 +2,13 @@
 
 set -euxo pipefail
 
-if [[ $PKG_NAME == "sparrow-json-reader"]]; then
+if [[ $PKG_NAME == "sparrow-json-reader" ]]; then
     cmake ${CMAKE_ARGS} \
         -DCMAKE_BUILD_TYPE=Release \
         -DBUILD_TESTS=OFF \
         -DCMAKE_INSTALL_PREFIX=$PREFIX \
         -DCMAKE_INSTALL_LIBDIR=lib \
-        -DCREATE_JSON_READER_TARGET=OFF \
+        -DCREATE_JSON_READER_TARGET=ON \
         $SRC_DIR
 else
     cmake ${CMAKE_ARGS} \
@@ -16,7 +16,7 @@ else
         -DBUILD_TESTS=OFF \
         -DCMAKE_INSTALL_PREFIX=$PREFIX \
         -DCMAKE_INSTALL_LIBDIR=lib \
-        -DCREATE_JSON_READER_TARGET=ON \
+        -DCREATE_JSON_READER_TARGET=OFF \
         $SRC_DIR
 fi
 
